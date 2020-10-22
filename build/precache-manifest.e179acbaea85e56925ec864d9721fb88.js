c7ea9bf0edd3bdb6a571500d3ca840fc871d3d60
hanbalahmed <72215552+hanbalahmed@users.noreply.github.com>
2020-10-22 21:29:42 +0500

    Add files via upload

---
+++ build/precache-manifest.e179acbaea85e56925ec864d9721fb88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e075f293f07c9b8b0eab45878e23ddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2f4fd416bc1e5412ced",
+    "url": "/static/css/main.209ead7f.chunk.css"
+  },
+  {
+    "revision": "18fc7fc9e4c0ca5e0c50",
+    "url": "/static/js/2.c2887715.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/static/js/2.c2887715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f4fd416bc1e5412ced",
+    "url": "/static/js/main.1b4849ed.chunk.js"
+  },
+  {
+    "revision": "9dd200754b771c2b0406",
+    "url": "/static/js/runtime-main.fd1ad065.js"
+  }
+]);
